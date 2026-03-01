e958109b6b5c3c1cc264df18b68a4b94ca3e9ee1
MatheussRiibeiro <matheusrf4@gmail.com>
2026-03-01 16:07:04 -0300

    refactor extraindo e lançando no bucket minio

--- src/script/extract/extraction.py
+++ src/script/extract/extraction.py
@@ -1,27 +1,42 @@
 import requests as r
-import pandas as pd
 import json
+import io
 import os
+from minio import Minio
+from dotenv import load_dotenv
+
+# Conectando no MinIO
+load_dotenv()
+
+client = Minio(
+    os.getenv("MINIO_ENDPOINT"),
+    access_key=os.getenv("MINIO_ACCESS_KEY"),
+    secret_key=os.getenv("MINIO_SECRET_KEY"),
+    secure=False
+)
+def upload_to_minio(data, filename):
+    data_bytes = json.dumps(data).encode('utf-8')
+    client.put_object(
+        "data-lake",
+        f"bronze/{filename}",
+        io.BytesIO(data_bytes),
+        length=len(data_bytes),
+        content_type="application/json"
+    )
+    print(f"{filename} enviado para o bronze!")
 
 #produtos para sampledata/products.json
 response = r.get('https://fakestoreapi.com/products')
 products = response.json()
-with open("sampledata/products.json", "w") as f:
-    json.dump(products, f, indent=4)
-print("Produtos salvos!")
+upload_to_minio(products, "products.json")
 
 
 # Usuários para sampledata/usuarios.json
 response = r.get('https://fakestoreapi.com/users')
 users = response.json()
-with open("sampledata/users.json", "w") as f:
-    json.dump(users, f, indent=4)
-print("Usuários salvos!")
+upload_to_minio(users, "users.json")
 
 # Carrinhos para sampledata/carrinho.json
 response = r.get('https://fakestoreapi.com/carts')
 carts = response.json()
-with open("sampledata/carts.json", "w") as f:
-    json.dump(carts, f, indent=4)
-print("Carrinhos salvos!")
-
+upload_to_minio(carts, "carts.json")
